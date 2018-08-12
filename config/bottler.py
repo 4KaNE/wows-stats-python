8f4967d5f54d7261c9f7d0c671df96e09c3c7c19
4KaNE <35267520+4KaNE@users.noreply.github.com>
2018-08-13 04:30:36 +0900

    check_wows_pathメソッドを追加

--- config/bottler.py
+++ config/bottler.py
@@ -1,9 +1,32 @@
 """open setting.html"""
+from os.path import isdir, isfile
 import webbrowser as web
 from bottle import route, run, post, template, request
 
 web.open('http://localhost:8080/setting')
+
+def check_wows_path(path :str) -> bool:
+    """
+    Check if the entered path is correct.
     
+    Parameters
+    ----------
+    path : str
+        Path of directory where WorldOfWarships.exe is located
+    
+    Returns
+    ----------
+    result : bool
+        True if path is correct, False otherwise.
+    """
+    result = False
+    exe_path = path + "/WorldOfWarships.exe"
+    if isfile(exe_path):
+        result = True
+
+    return result
+
+
 @route('/setting')
 def setting():
     return template('setting.tpl', errorMessage="")
@@ -11,15 +34,21 @@ def setting():
 @post('/setting')
 def do_setting():
     wows_path = request.forms.wowsPath
+
     app_id = request.forms.appId
     region = request.forms.region
     ign = request.forms.ign
+    wows_path = wows_path.replace("\\", "/")
     print(wows_path)
     print(app_id)
     print(region)
     print(ign)
 
     success = False
+    error_list = []
+    if not check_wows_path(wows_path):
+        error_list.append("入力されたWoWsディレクトリのパスが不正です。")
+
     error = "存在しないアプリケーションキーです"
     error_message = "ERROR!: {}".format(error)
 
@@ -51,3 +80,4 @@ def do_setting():
 
 
 run(host='localhost', port=8080, debug=True)
+
