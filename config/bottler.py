8a2b9c308402b689afa5147d7619210264dd1840
4KaNE <35267520+4KaNE@users.noreply.github.com>
2018-08-12 22:24:52 +0900

    bottler.pyにPOST通信のひな型を追加

--- config/bottler.py
+++ config/bottler.py
@@ -1,7 +1,27 @@
-from bottle import route, run
+"""open setting.html"""
+import webbrowser as web
+from bottle import route, run, post, request
+
+web.open('http://localhost:8080/setting')
     
 @route('/setting')
-def hello():
-    return "Hello World!"
+def setting():
+    return """
+    <form action="/setting" method="post">
+    Username: <input name="username" type="text" />
+    Password: <input name="password" type="password" />
+    <input value="Login" type="submit" />
+    </form>
+    """
+
+@post('/setting')
+def do_setting():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    print(username)
+    print(password)
+
+    return "success!"
+
     
 run(host='localhost', port=8080, debug=True)
