5836f7e9de0ffb429b03fe116917bf9b44d51c3c
4KaNE <35267520+4KaNE@users.noreply.github.com>
2018-08-26 21:38:47 +0900

    vue.js実装
    cssがきいてない

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 """test"""
 import os
-from json import load, dump
+from datetime import datetime
+from time import sleep
+from json import load, dumps
 from json.decoder import JSONDecodeError
-from bottle import route, run, static_file
+
+from bottle import route, run, static_file, request, Bottle, abort
 from bottle import TEMPLATE_PATH, jinja2_template as template
 
+from gevent.pywsgi import WSGIServer
+from geventwebsocket import WebSocketError
+from geventwebsocket.handler import WebSocketHandler
 
+APP = Bottle()
+SERVER = WSGIServer(("0.0.0.0", 8080), APP, handler_class=WebSocketHandler)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 #STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+"""
 @route('/')
 def hoge():
-    """
-    test
-    """
     json_path = "{}/application/data.json".format(BASE_DIR)
     with open(json_path, 'r', encoding="utf-8_sig") as json_file:
         data = load(json_file)
@@ -21,10 +27,49 @@ def hoge():
     flag = 1
 
     return template('static/index', data=data, flag=flag)
+"""
 
 @route('/static/<file_path:path>')
 def static(file_path):
     """css"""
-    return static_file(file_path, root='./static')
+    print("AAAAAAAA")
+    return static_file(file_path, root='{}/static'.format(BASE_DIR))
 
-run(host='localhost', port=8080,  reloader=True, debug=True)
+@APP.route('/websocket')
+def handle_websocket():
+    """
+    WebSocket Handler
+    Return count once every 3 seconds
+    """
+    websocket = request.environ.get('wsgi.websocket')
+
+    json_path = "{}/application/data.json".format(BASE_DIR)
+    with open(json_path, 'r', encoding="utf-8_sig") as json_file:
+        data = load(json_file)
+
+    if not websocket:
+        abort(400, 'Expected WebSocket request.')
+
+    while True:
+        try:
+            handler = websocket.handler
+            for client in handler.server.clients.values():
+                now_time = datetime.strftime(datetime.now(), '%H:%M:%S')
+                print(now_time)
+                client.ws.send(dumps(data))
+
+        except WebSocketError:
+            break
+
+        sleep(10)
+
+@APP.route('/')
+def top():
+    """
+    Returning WebSocket client when accessing localhost:8080/
+    """
+    return static_file('static/index.html', root='./')
+
+SERVER.serve_forever()
+
+#run(host='localhost', port=8080,  reloader=True, debug=True)
