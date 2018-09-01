55a1f6bb7307b574fbe908315f2a7c031f5f4279
4KaNE <35267520+4KaNE@users.noreply.github.com>
2018-09-01 23:43:51 +0900

    docstring

--- application/wows_stats.py
+++ application/wows_stats.py
@@ -2,6 +2,12 @@
 
 class WoWsStats():
     """
+    Class for storing information to be displayed 
+    on the client in json format
     """
     def __init__(self):
-        pass
+        self.stats_dict = {}
+        self.friends_stats_list = []
+        self.enemy_stats_list = []
+
+    
