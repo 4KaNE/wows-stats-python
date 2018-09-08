b67895c0357c44a808222c969f7774e801862413
4KaNE <35267520+4KaNE@users.noreply.github.com>
2018-09-08 20:47:25 +0900

    main.pyにwows_statsを連結

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """Server"""
 import os
+import configparser
 from datetime import datetime
 from time import sleep
 from json import load, dumps
@@ -10,6 +11,47 @@ from bottle import route, run, static_file, request, Bottle, abort
 from gevent.pywsgi import WSGIServer
 from geventwebsocket import WebSocketError
 from geventwebsocket.handler import WebSocketHandler
+
+from application import wows_api_wrapper, ships_info, wows_stats
+
+INIFILE = configparser.SafeConfigParser()
+INIFILE.read('./config/config.ini', 'UTF-8')
+app_id = INIFILE["config"]["application_id"]
+region = INIFILE["config"]["region"]
+SI = ships_info.ShipInfo(app_id, region)
+WAW = wows_api_wrapper.APIWrapper(app_id, region)
+
+def test(ArenaInfo):
+    wst = wows_stats.WoWsStats()
+    
+    for vehicle in ArenaInfo["vehicles"]:
+        wst.init_user_dict()
+        ign = vehicle["name"]
+        wst.add_ign(ign)
+        account_id = WAW.fetch_accountid(ign)
+        wst.add_userid(account_id)
+    
+        clan_id = WAW.fetch_clan_id(account_id)
+        clan_tag = None if clan_id is None else WAW.fetch_clan_tag(clan_id)
+        wst.add_clan(clan_tag)
+    
+        ship_id = vehicle["shipId"]
+        wst.add_ship_info(ship_id, SI.tier(ship_id), SI.name(
+            ship_id), SI.nation(ship_id), SI.ship_type(ship_id))
+    
+        personal_data = WAW.fetch_personal_data(account_id)
+        ship_stats, rank_info = None if personal_data is None else WAW.fetch_ship_stats(
+            account_id, ship_id), WAW.fetch_rank_stats(account_id)
+        wst.add_personal_data(personal_data)
+        wst.add_ship_stats(ship_stats)
+        wst.add_rank(rank_info)
+    
+        wst.update_tmplist(vehicle["relation"])
+    
+    wst.sort_tmplist()
+    data = wst.create_stats_dict(ArenaInfo["mapDisplayName"])
+    
+    return data
 
 APP = Bottle()
 SERVER = WSGIServer(("0.0.0.0", 8080), APP, handler_class=WebSocketHandler)
@@ -29,10 +71,11 @@ def handle_websocket():
     Return data once every 5 seconds
     """
     websocket = request.environ.get('wsgi.websocket')
+    print("aaaa")
 
-    json_path = "{}/application/data.json".format(BASE_DIR)
-    with open(json_path, 'r', encoding="utf-8_sig") as json_file:
-        data = load(json_file)
+    #json_path = "{}/application/data.json".format(BASE_DIR)
+    #with open(json_path, 'r', encoding="utf-8_sig") as json_file:
+    #    data = load(json_file)
 
     if not websocket:
         abort(400, 'Expected WebSocket request.')
@@ -40,9 +83,14 @@ def handle_websocket():
 
     while True:
         count += 1
-        if count % 10 == 1:
+        if count % 10 != 0:
             print(count)
             continue
+
+        with open("tempArenaInfo.json", "r", encoding="utf-8_sig") as json_file:
+            ArenaInfo = load(json_file)
+
+        data = test(ArenaInfo)
         try:
             handler = websocket.handler
             for client in handler.server.clients.values():
@@ -60,4 +108,5 @@ def top():
     """
     return static_file('static/index.html', root='./')
 
+print("bbbb")
 SERVER.serve_forever()
